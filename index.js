106e7b39b0482aad52870cb1933807fb1cc5fab9
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-15 23:57:53 -0400

    Add functionality to GET and POST blogs

--- index.js
+++ index.js
@@ -1,16 +1,72 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+class Blog extends Model { }
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
   }
-}
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
 
-main()
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+app.get('/api/blogs', async (req, res) => {
+  // const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+  const blogs = await Blog.findAll();
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  console.log(req.body)
+  try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+//QUERY TEST CODE
+// const main = async () => {
+//   try {
+//     await sequelize.authenticate()
+//     const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+//     console.log(blogs)
+//     sequelize.close()
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error)
+//   }
+// }
+
+// main()
