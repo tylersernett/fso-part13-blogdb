04fb04ddd10733ef13e4843cd07ae6b77c9dbf63
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-17 00:31:41 -0400

    Require token to POST new blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
 
-const { Blog } = require('../models')
+const { SECRET } = require('../util/config')
+const { Blog, User } = require('../models')
 
 router.get('/', async (req, res) => {
   // const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
@@ -16,14 +18,25 @@ const blogFinder = async (req, res, next) => {
   next()
 }
 
-router.post('/', async (req, res) => {
-  console.log(req.body)
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+      console.log(req.decodedToken)
+    } catch {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
+router.post('/', tokenExtractor, async (req, res) => {
   try {
-    //default to first user
-    const user = await User.findOne()    
-    const blog = await Blog.create({...req.body, userId: user.id})
-    
-    // const blog = await Blog.create(req.body)
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.create({ ...req.body, userId: user.id})
     res.json(blog)
   } catch (error) {
     return res.status(400).json({ error })
@@ -37,7 +50,7 @@ router.delete('/:id', blogFinder, async (req, res) => {
   res.status(204).end()
 })
 
-router.put('/:id', blogFinder, async(req, res) => {
+router.put('/:id', blogFinder, async (req, res) => {
   if (req.blog) {
     req.blog.likes = req.body.likes
     await req.blog.save()
