e107ef28763712e6ab2e123efbbf98d5b9841036
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-21 23:45:47 -0400

    Update hours

--- commands.sql
+++ commands.sql
@@ -1,4 +1,4 @@
--- hours spent: 2 + 11:00 = 2
+-- hours spent: 2 + 1.5 + 1 + 2.5 + 1 = 8
 CREATE TABLE blogs (
     id SERIAL PRIMARY KEY,
     author text,
@@ -12,3 +12,44 @@ VALUES ('Test Testerson', 'www.test.com', 'The First Test');
 
 INSERT INTO blogs (author, url, title, likes)
 VALUES ('Cool Guy', 'www.cool.com', 'Another Test', 2);
+
+INSERT INTO blogs (author, url, title, likes)
+VALUES ('Cool Guy', 'www.cool.com/2', 'Another Test 2', 3);
+
+INSERT INTO blogs (author, url, title, likes)
+VALUES ('Mr Cool', 'www.cool.com/2222', 'Wow its cool', 22);
+
+INSERT INTO blogs (author, url, title, likes)
+VALUES ('New Author', 'www.cool.com/new', 'New kid on the block', 1);
+
+
+INSERT INTO users (username, name)
+VALUES ('test', 'John Doe');
+
+
+{
+	"author": "Mr Cool",
+	"url": "mrcool.com",
+	"title": "Auth test"
+}
+
+{
+	"username":"testup",
+	"password":"secret"
+}
+
+--create user
+{
+	"username":"test@test.com",
+	"name": "T Testy",
+	"password":"secret"
+}
+
+--create blog
+{
+	"author": "Mr Cool",
+	"url": "mrcool.com",
+	"title": "Year test",
+	"likes": 2,
+	"year": 1985
+}
