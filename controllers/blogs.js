8e6e955fa685079bb51990f26455704296760932
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-23 14:14:52 -0400

    Extract tokenExtractor to middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,6 @@
 const router = require('express').Router()
-const jwt = require('jsonwebtoken')
 const { Op } = require('sequelize')
-
-const { SECRET } = require('../util/config')
+const {tokenExtractor} = require('../util/middleware')
 const { Blog, User } = require('../models')
 
 router.get('/', async (req, res) => {
@@ -44,21 +42,6 @@ router.get('/', async (req, res) => {
 //MIDDLEWARE
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
-  next()
-}
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-      console.log(req.decodedToken)
-    } catch {
-      return res.status(401).json({ error: 'token invalid' })
-    }
-  } else {
-    return res.status(401).json({ error: 'token missing' })
-  }
   next()
 }
 
