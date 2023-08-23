9d7552e31b629164b15405d02d2a6b4ad8cb8acb
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-23 14:38:31 -0400

    Add route to update reading list

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -1,10 +1,28 @@
 const router = require('express').Router()
+const { tokenExtractor } = require('../util/middleware')
 
-const { ReadingList } = require('../models')
+const { ReadingList, User } = require('../models')
 
 router.post('/', async (req, res) => {
   const readingList = await ReadingList.create(req.body)
   res.json(readingList)
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  const readingList = await ReadingList.findByPk(req.params.id)
+
+  if (!readingList) {
+    return res.status(404).json({ error: 'Reading List ID not found' })
+  }
+  if (user.id !== readingList.userId) {
+    return res.status(403).json({ error: 'Users may only modify their own reading list' })
+  }
+
+  readingList.read = req.body.read
+  await readingList.save()
+
+  return res.json(readingList)
 })
 
 module.exports = router
