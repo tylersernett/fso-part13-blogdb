e01dc7d511506fa9b18d459789f64732152a05f7
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-25 23:38:33 -0400

    Update commands

--- commands.sql
+++ commands.sql
@@ -1,4 +1,4 @@
--- hours spent: 2 + 1.5 + 1 + 2.5 + 1 = 8
+-- hours spent: 2 + 1.5 + 1 + 2.5 + 1 + 2 + 1.5 + 1.5 = 13
 CREATE TABLE blogs (
     id SERIAL PRIMARY KEY,
     author text,
@@ -24,7 +24,7 @@ VALUES ('New Author', 'www.cool.com/new', 'New kid on the block', 1);
 
 
 INSERT INTO users (username, name)
-VALUES ('test', 'John Doe');
+VALUES ('jd@test.com', 'John Doe');
 
 
 {
@@ -44,6 +44,16 @@ VALUES ('test', 'John Doe');
 	"name": "T Testy",
 	"password":"secret"
 }
+{
+	"username":"jd@test.com",
+	"name": "John Doe",
+	"password":"secret"
+}
+{
+	"username":"x@test.com",
+	"name": "Mr X",
+	"password":"secret"
+}
 
 --create blog
 {
@@ -52,4 +62,59 @@ VALUES ('test', 'John Doe');
 	"title": "Year test",
 	"likes": 2,
 	"year": 1985
+}
+
+{
+	"author": "Mr Cool",
+	"url": "mrcool.com",
+	"title": "The 90s",
+	"likes": 4,
+	"year": 1999
+}
+
+{
+	"author": "The Dude",
+	"url": "thedude.com",
+	"title": "White Russians",
+	"likes": 10,
+	"year": 1997
+}
+
+{
+	"author": "Miles Davis",
+	"url": "jazz.com",
+	"title": "Being Cool",
+	"likes": 13,
+	"year": 1990
+}
+
+{
+	"author": "Miles Davis",
+	"url": "jazz.com",
+	"title": "Being Even Cooler",
+	"likes": 14,
+	"year": 1999
+}
+
+{
+	"author": "Miles Davis",
+	"url": "jazz.com",
+	"title": "This Should Fail",
+	"likes": 1,
+	"year": 2000
+}
+
+--login
+{
+	"username":"x@test.com",
+	"password":"secret"
+}
+
+--readingLists POST
+insert into reading_lists (user_id, blog_id) values (4, 1);
+insert into reading_lists (user_id, blog_id) values (4, 3);
+
+{
+  "blogId": 4,
+  "userId": 4
 }
