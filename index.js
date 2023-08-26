d7399555ee08f85c79093393740d5e3c0f8a26d2
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-25 23:41:58 -0400

    Add logout route

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { connectToDatabase } = require('./util/db')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const logoutRouter = require('./controllers/logout')
 const authorsRouter = require('./controllers/authors')
 const readingListsRouter = require('./controllers/readingLists')
 
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/logout', logoutRouter)
 app.use('/api/authors', authorsRouter)
 app.use('/api/readinglists', readingListsRouter)
 
@@ -25,6 +27,7 @@ app.use((err, req, res, next) => {
   if (err.name.startsWith('Sequelize')) {
     return res.status(400).send({ error: err.errors[0].message });
   }
+  //this is now handled by the sequelize error message above
   // if (err.name === 'SequelizeValidationError') {
   //   if (err.errors[0].message === 'Validation isEmail on username failed') {
   //     return res.status(400).send({ error: 'username must be an email address' })
